189741981b276dfd1e358f3be1c370f3b5297b6b
cyrilpanicker <cyrilpanicker@gmail.com>
2016-05-10 17:39:07 +0530

    added changes

---
+++ app/todo.component.js
@@ -0,0 +1,13 @@
+"use strict";
+var TodoComponent = (function () {
+    function TodoComponent() {
+        this.todo = {
+            id: 1,
+            text: 'todo1',
+            complete: false
+        };
+    }
+    return TodoComponent;
+}());
+exports.TodoComponent = TodoComponent;
+//# sourceMappingURL=todo.component.js.map
