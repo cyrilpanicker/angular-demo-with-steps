e50a7e767f88363c0823c7eb50331b4c80dd5369
cyrilpanicker <cyrilpanicker@gmail.com>
2016-05-10 17:40:45 +0530

    adde changes

--- app/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var todo_component_1 = require('./todo.component');
-platform_browser_dynamic_1.bootstrap(todo_component_1.TodoComponent);
-//# sourceMappingURL=main.js.map
