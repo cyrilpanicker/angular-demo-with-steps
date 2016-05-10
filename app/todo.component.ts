718df9d74112fd649493ebb8d53cc25a262d03a5
cyrilpanicker <cyrilpanicker@gmail.com>
2016-05-10 17:30:48 +0530

    added changes

--- app/todo.component.ts
+++ app/todo.component.ts
@@ -1,4 +1,20 @@
 import {Todo} from './todo.model';
+import {Component} from '@angular/core';
+
+@Component({
+    selector:'todo',
+    template:`
+        <h2>
+            {{todo.text}} - 
+            <span [hidden]="todo.complete" >pending</span>
+            <span [hidden]="!todo.complete" >complete</span>
+        </h2>
+        <div>
+            Text : <input [value]="todo.text" /><br/>
+            Complete : <input type="checkbox" [checked]="todo.complete" />
+        </div>
+    `
+})
 
 export class TodoComponent{
     todo:Todo={
